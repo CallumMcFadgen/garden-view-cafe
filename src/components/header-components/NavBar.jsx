6933cf252745ff64f301d615f6292c47f35ce1e9
CallumMcFadgen <callum.mcfadgen@gmail.com>
2019-08-24 10:41:31 +1200

    navbar styled with heading and working links

--- src/components/header-components/NavBar.jsx
+++ src/components/header-components/NavBar.jsx
@@ -6,16 +6,19 @@ class NavBar extends Component {
   render() {
     return (
       <header className="header-ui">
+        <div className="header-title">
+          The Garden View Cafe
+        </div>
         <div className="navbar-links">
-          <span className="navButton"><Link to="">Home</Link></span>
+          <span><Link className ="link" to="">Home</Link></span>
           <span className="navbar-spacers"> / </span>
-          <span className="navButton"><Link to="./MenuPage">Menu</Link></span>
+          <span><Link className ="link" to="./MenuPage" >Menu</Link></span>
           <span className="navbar-spacers"> / </span>
-          <span className="navButton"><Link to="./EventsPage">Events</Link></span>
+          <span><Link className ="link" to="./EventsPage">Events</Link></span> 
           <span className="navbar-spacers"> / </span>
-          <span className="navButton"><Link to="./GalleryPage">Gallery</Link></span>
+          <span><Link className ="link" to="./GalleryPage">Gallery</Link></span>
           <span className="navbar-spacers"> / </span>
-          <span className="navButton"><Link to="./ContactPage">Contact</Link></span>
+          <span><Link className ="link" to="./ContactPage">Contact</Link></span>
         </div>
       </header>
     )
