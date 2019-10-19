eb55e0388d4d4f9c65b06b2720077f4fb6669bbf
CallumMcFadgen <callum.mcfadgen@gmail.com>
2019-10-20 11:34:18 +1300

    tidying up, bug fixing all tables finished, fresh dependencies added

--- src/components/tables/events/spring-events.jsx
+++ src/components/tables/events/spring-events.jsx
@@ -4,26 +4,33 @@ import { Table } from 'reactstrap';
 import moment from "moment";
 
 class SpringTable extends React.Component {
+  _isMounted = false;           // variable to hold the components mounted state
 
   constructor() {
     super();
     this.state = ({
       data: ''            // set a null variable for data
     })
-  }
+  };
 
+  
   componentDidMount() {
+    this._isMounted = true;           // set mounted state to true
+
     fetch('http://localhost:4200/api/events')    // get data 
       .then(res => res.json())   // json the results
       .then(data => {
+        if (this._isMounted) {                  // if the component is mounted, do stuff
         this.setState({    // set to storage?
           data: data
-        })
+        })}
       })
       .catch(err => {
         console.log(err);
       })
-  }
+  };
+
+
   render() {
     console.log(this.state.data)
     if (this.state.data !== '') {     // if there is data, do stuff
@@ -32,8 +39,8 @@ class SpringTable extends React.Component {
           <thead>
             <tr>
               <th></th>
+              <th>Time & Date</th>
               <th>Name</th>
-              <th>Date</th>
               <th>Description</th>
               <th>Price</th>
             </tr>
@@ -41,10 +48,10 @@ class SpringTable extends React.Component {
           <tbody>
             {this.state.data.map((item, index) => {  
               return (
-                <tr>
+                <tr key={item.event_id} value="event_id">
                   <th scope="row"></th>
-                  <td>{item.event_name}</td>
                   <td>{moment(item.event_date_time).format("h:mm a DD-MM-YYYY")}</td>
+                  <td>{item.event_name}</td>
                   <td>{item.event_description}</td>
                   <td>${item.event_price}</td>
                 </tr>
@@ -57,7 +64,12 @@ class SpringTable extends React.Component {
     else {
       return null;
     }
-  }
-}
+  };
+
+
+  componentWillUnmount() {            //reset components mounted state on unmount
+    this._isMounted = false;
+  };
+};
 
 export default SpringTable;
--- src/components/tables/events/summer-events.jsx
+++ src/components/tables/events/summer-events.jsx
@@ -1,51 +1,75 @@
+// Package Dependences
 import React from 'react';
 import { Table } from 'reactstrap';
+import moment from "moment";
 
-const SummerTable = (props) => {
-  return (
-    <Table>
-      <thead>
-        <tr className="test">
-          <th></th>
-          <th>Name</th>
-          <th>Date</th>
-          <th>Description</th>
-          <th>Price</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row"></th>
-          <td>Name</td>
-          <td>Date</td>
-          <td>Desc</td>
-          <td>Price</td>
-        </tr>
-        <tr>
-          <th scope="row"></th>
-          <td>Name</td>
-          <td>Date</td>
-          <td>Desc</td>
-          <td>Price</td>
-        </tr>
-        <tr>
-          <th scope="row"></th>
-          <td>Name</td>
-          <td>Date</td>
-          <td>Desc</td>
-          <td>Price</td>
-        </tr>
-        <tr>
-          <th scope="row"></th>
-          <td>Name</td>
-          <td>Date</td>
-          <td>Desc</td>
-          <td>Price</td>
-        </tr>
+class SummerTable extends React.Component {
+  _isMounted = false;           // variable to hold the components mounted state
 
-      </tbody>
-    </Table>
-  );
-}
+  constructor() {
+    super();
+    this.state = ({
+      data: ''            // set a null variable for data
+    })
+  };
+
+  
+  componentDidMount() {
+    this._isMounted = true;           // set mounted state to true
+
+    fetch('http://localhost:4200/api/events')    // get data 
+      .then(res => res.json())   // json the results
+      .then(data => {
+        if (this._isMounted) {                  // if the component is mounted, do stuff
+        this.setState({    // set to storage?
+          data: data
+        })}
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  };
+
+
+  render() {
+    console.log(this.state.data)
+    if (this.state.data !== '') {     // if there is data, do stuff
+      return (
+        <Table>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Time & Date</th>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.data.map((item, index) => {  
+              return (
+                <tr key={item.event_id} value="event_id">
+                  <th scope="row"></th>
+                  <td>{moment(item.event_date_time).format("h:mm a DD-MM-YYYY")}</td>
+                  <td>{item.event_name}</td>
+                  <td>{item.event_description}</td>
+                  <td>${item.event_price}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </Table>
+      )
+    }
+    else {
+      return null;
+    }
+  };
+
+
+  componentWillUnmount() {            //reset components mounted state on unmount
+    this._isMounted = false;
+  };
+};
 
 export default SummerTable;
